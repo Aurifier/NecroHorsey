99023fe68a1320363c76751c83d96b4dc3778e1e
Aurifier <reed.995@osu.edu>
2014-06-21 09:06:20 -0400

    Initial commit.

---
+++ src/main/java/com/caffeinecraft/necrohorsey/NecroHorsey.java
@@ -0,0 +1,9 @@
+package com.caffeinecraft.necrohorsey;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by drew on 6/21/14.
+ */
+public final class NecroHorsey extends JavaPlugin {
+}
